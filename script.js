dba225868fdcbf7c920d95b8add40177ff3c42ba
Kaushik Ram <22810981+Kaushik-A-Ram@users.noreply.github.com>
2025-04-10 17:22:31 +1000

    created movie class

---
+++ script.js
@@ -0,0 +1,9 @@
+    // creating movie class
+    class movie {
+        constructor(movieId, title, year, rating) {
+            this.movieId = movieId;
+            this.title = title;
+            this.year = year;
+            this.rating = rating;
+        }
+    }
