dbfff0dd6c62f39b131fb9c6ad71bf8c1907cd9a
Kaushik Ram <22810981+Kaushik-A-Ram@users.noreply.github.com>
2025-04-11 13:25:57 +1000

    failed, dont know what i am doing

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // creating movie class
-class movie {
+class Movie {
   constructor(movieId, title, year, rating) {
     this.movieId = movieId;
     this.title = title;
@@ -7,3 +7,19 @@ class movie {
     this.rating = rating;
   }
 }
+
+
+const id = document.getElementById("#id");
+const title = document.getElementById("#title");
+const year = document.getElementById("#year");
+const rating = document.getElementById("#rating");
+const form = document.querySelector("#movieForm");
+
+form.addEventListener("submit", e=> {
+  e.preventDefault();
+  const formData = new FormData(e.target)
+  const id = new FormData();
+  const title = new FormData();
+  const year = new FormData();
+  const rating = new FormData();
+})
