4989531415937c3242d701bd2f79cbf7e8f3d550
Kaushik Ram <22810981+Kaushik-A-Ram@users.noreply.github.com>
2025-04-11 11:54:04 +1000

    created movie form

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-    // creating movie class
-    class movie {
-        constructor(movieId, title, year, rating) {
-            this.movieId = movieId;
-            this.title = title;
-            this.year = year;
-            this.rating = rating;
-        }
-    }
+// creating movie class
+class movie {
+  constructor(movieId, title, year, rating) {
+    this.movieId = movieId;
+    this.title = title;
+    this.year = year;
+    this.rating = rating;
+  }
+}
